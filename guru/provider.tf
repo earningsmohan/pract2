a93800d3bdea76fd84f951cc1ec1a705616288f3
earningsmohan <71962475+earningsmohan@users.noreply.github.com>
2021-09-17 10:57:44 +0530

    Delete guru directory

--- guru/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "google" {
-  credentials = file("/Users/mokshithreddy/Downloads/terrtest1/terraformgke-323516-1388e0b2ea54.json")
-  project     = "terraformgke-323516"
-  region      = "us-central1"
-  version     = "2.5.1"
-}
